4de48bdb2443eb983beb38281af558360c655712
BDA228 <deniska_belodedov@mail.ru>
2026-02-11 12:37:26 +0700

    Тест

--- composeApp/src/commonMain/kotlin/com/example/track_me_mobile/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/track_me_mobile/App.kt
@@ -32,7 +32,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Clickпшогнпгрргрг!")
+                Text("Clickпшогнпгрргргыпып!")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
