25f52de9eb841971e2d1b25cecedaac5596d0c56
mirekSxd <l.ulitsa22@yandex.ru>
2026-02-11 13:01:34 +0300

    Update App.kt
    Test github desktop

--- composeApp/src/commonMain/kotlin/com/example/track_me_mobile/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/track_me_mobile/App.kt
@@ -32,7 +32,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Cl!")
+                Text("Cltrfygu!")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
