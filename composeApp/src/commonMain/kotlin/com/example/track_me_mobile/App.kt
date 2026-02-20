f66a6bde32b124dc60030cefcffe7d4361eae5eb
mirekSxd <l.ulitsa22@yandex.ru>
2026-02-20 15:25:09 +0300

    Страница отчетности

--- composeApp/src/commonMain/kotlin/com/example/track_me_mobile/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/track_me_mobile/App.kt
@@ -4,15 +4,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.track_me_mobile.features.meetings.presentation.MeetingNavigation
+import com.example.track_me_mobile.features.streams.presentation.StreamsListScreen
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        // Вызываем навигатор, который сам решит, какой экран показать
-        // В файле App.kt
-        MeetingNavigation(onBack = {
-            // Здесь логика возврата, например: navController.popBackStack(
-        })
+        StreamsListScreen()
+
+        }
     }
+
+@Composable
+fun StreamsListScreen() {
+    TODO("Not yet implemented")
 }
